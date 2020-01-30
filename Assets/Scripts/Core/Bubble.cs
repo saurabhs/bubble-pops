4896636de7c3c2f74c7155951ab6925cc8eb0aaa
Saurabh S <saurabhs@mailbox.org>
2020-01-31 00:34:15 +0530

    Add Bubble Prefabs (2-2048)

---
+++ Assets/Scripts/Core/Bubble.cs
@@ -0,0 +1,43 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace BubblePops.Core
+{
+    public enum EType
+    {
+        Two = 2,
+        Four = 4,
+        Eight = 8,
+        Sixteen = 16,
+        ThirtyTwo = 32,
+        SixtyFour = 64,
+        OneTwentyEight = 128,
+        TwoFiftySix = 256,
+        FiveTwelve = 512,
+        TenTwentyFour = 1024,
+        TwentyFortyEight = 2048
+    }
+
+    public class Bubble : MonoBehaviour
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [SerializeField] private BubblePops.Core.EType _value;
+
+        [SerializeField] private TextMeshProUGUI _label;
+
+        private void Awake()
+        {
+            _label.text = ((int)_value).ToString();
+        }
+
+        private void OnEnable()
+        {
+        }
+
+        private void OnDisable()
+        {
+        }
+    }
+}
