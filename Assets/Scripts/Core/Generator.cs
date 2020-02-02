38f73e3356c938ffa4a0e85f0c285079b34f9d5e
Saurabh S <saurabhs@mailbox.org>
2020-02-02 18:54:21 +0530

    Add bubble generation based on hexgrid

--- Assets/Scripts/Core/Generator.cs
+++ Assets/Scripts/Core/Generator.cs
@@ -10,71 +10,60 @@ namespace BubblePops.Core
         /// </summary>
         [SerializeField] private GameObject[] _bubblesPrefab = null;
 
-        /// <summary>
-        /// max rows
-        /// </summary>
-        [SerializeField] private int _rows = 6;
+        // /// <summary>
+        // /// max rows
+        // /// </summary>
+        // [SerializeField] private int _rows = 6;
 
-        /// <summary>
-        /// max coloumn
-        /// </summary>
-        [SerializeField] private int _coloumns = 4;
+        // /// <summary>
+        // /// max coloumn
+        // /// </summary>
+        // [SerializeField] private int _coloumns = 4;
 
-        /// <summary>
-        /// parent object to group the bubbles
-        /// </summary>
-        [SerializeField] private GameObject _parent = null;
+        [SerializeField] private Grid _grid = null;
 
-        /// <summary>
-        /// horizonatal and vertical offset for placing bubbles
-        /// </summary>
-        [SerializeField] private Vector2 _offset = new Vector2(0.83f, -0.7249f);
+        // /// <summary>
+        // /// horizonatal and vertical offset for placing bubbles
+        // /// </summary>
+        // [SerializeField] private Vector2 _offset = new Vector2(0.83f, -0.7249f);
 
-        /// <summary>
-        /// offset for alternate rows
-        /// </summary>
-        [SerializeField] private float _alternateOffset = 0.4113f;
+        // /// <summary>
+        // /// offset for alternate rows
+        // /// </summary>
+        // [SerializeField] private float _alternateOffset = 0.4113f;
 
         /// <summary>
         /// list of generated bubbles
         /// </summary>
         private HashSet<GameObject> _bubbles = new HashSet<GameObject>();
 
-
         public GameObject[] BubblePrefab => _bubblesPrefab;
 
         public HashSet<GameObject> Bubbles => _bubbles;
 
-        private void OnEnable()
-        {
-            Setup();
-        }
+        private void Start() => Setup();
 
         private void Setup()
         {
-            for (var j = 0; j < _coloumns; j++)
+            var rows = _grid.Rows;
+            var coloumns = _grid.Coloumn;
+            var index = 0;
+
+            for (var j = 0; j < coloumns; j++)
             {
-                for (var i = 0; i < _rows; i++)
+                for (var i = 0; i < rows; i++)
                 {
-                    _bubbles.Add(CreateBubble(i, j, j % 2 == 1 ? _alternateOffset : 0));
+                    var cell = _grid.GridData[index++].transform;
+                    var bubble = CreateBubble(i, j, cell.position, cell);
                 }
             }
         }
 
-        private GameObject CreateBubble(int row, int coloumn, float offset = 0f)
+        private GameObject CreateBubble(int row, int coloumn, Vector2 position, Transform parent)
         {
-            var position = new Vector2((_offset.x * row) + offset, _offset.y * coloumn);
-            var bubble = Instantiate(GetRandomBubble(), position, Quaternion.identity, _parent.transform);
-            bubble.transform.localPosition = position;
-            bubble.name = $"Bubble|{row}x{coloumn}";
-
+            var bubble = Instantiate(GetRandomBubble(), position, Quaternion.identity, parent);
+            bubble.name = $"Bubble_{row}_{coloumn}";
             return bubble;
-        }
-
-        public void CreateNewRow(int coloumn)
-        {
-            for (var i = 0; i < _rows; i++)
-                _bubbles.Add(CreateBubble(i, coloumn, coloumn % 2 == 1 ? _alternateOffset : 0));
         }
 
         private GameObject GetRandomBubble() => _bubblesPrefab[UnityEngine.Random.Range(0, _bubblesPrefab.Length / 2)];
