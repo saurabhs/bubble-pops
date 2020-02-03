648107fff7b557c635e9de59ccb8931ee8fe3a37
Saurabh S <saurabhs@mailbox.org>
2020-02-03 21:21:46 +0530

    Add multilevel grouping

--- Assets/Scripts/Core/MatchColors.cs
+++ Assets/Scripts/Core/MatchColors.cs
@@ -14,8 +14,6 @@ namespace BubblePops.Core
         /// <summary>
         /// 
         /// </summary>
-        private List<GameObject> _similarBubbles = new List<GameObject>();
-
         private List<GameObject> _neighbours = new List<GameObject>();
 
         private void Awake() => _cell = GetComponent<Cell>();
@@ -44,11 +42,17 @@ namespace BubblePops.Core
 
         public void OnPointerClick(PointerEventData eventData)
         {
-            var resultBubble = OnMatchColours();
-            if (resultBubble == -1)
+            GroupBubbles();
+        }
+
+        private void GroupBubbles()
+        {
+            var similarBubbles = OnMatchColours();
+            if (similarBubbles.Count < 2)
                 return;
 
-            if (resultBubble >= (int)EType.TwentyFortyEight)
+            var resultValue = (int)_cell.bubble.Type * (int)Mathf.Pow(2, similarBubbles.Count - 1);
+            if (resultValue >= (int)EType.TwentyFortyEight)
             {
                 StartCoroutine(FindObjectOfType<Generator>().Reset());
             }
@@ -58,38 +62,34 @@ namespace BubblePops.Core
                 var row = int.Parse(ss[1]);
                 var coloumn = int.Parse(ss[2]);
 
-                for (var i = 0; i < _similarBubbles.Count; i++)
+                for (var i = 0; i < similarBubbles.Count; i++)
                 {
-                    var cell = _similarBubbles[i].GetComponent<Cell>();
+                    var cell = similarBubbles[i].GetComponent<Cell>();
                     cell.Neighbours.Clear();
                     Destroy(cell.bubble.gameObject);
                 }
-                _similarBubbles.Clear();
+                similarBubbles.Clear();
 
-                var prefab = Resources.Load($"Prefabs/Bubble{resultBubble}") as GameObject;
+                var prefab = Resources.Load($"Prefabs/Bubble{resultValue}") as GameObject;
                 var newBubble = Generator.CreateBubble(prefab, row, coloumn, _cell.gameObject.transform.position);
                 newBubble.name = $"Bubble_{row}_{coloumn}";
 
                 _cell.bubble = newBubble.GetComponent<Bubble>();
                 _cell.SetNeighbours();
                 FindObjectOfType<Grid>().UpdateGrid();
+                
+                GroupBubbles();
             }
         }
 
-        private int OnMatchColours()
+        private List<GameObject> OnMatchColours()
         {
-            _similarBubbles = new List<GameObject>();
-
             _neighbours = new List<GameObject>();
             _neighbours.Add(_cell.gameObject);
             for (int i = 0; i < _cell.Neighbours.Count; i++)
                 _neighbours.Add(_cell.Neighbours[i]);
 
-            _similarBubbles = GetMatchingNeighbours(_cell, _cell.bubble.Type);
-            if (_similarBubbles.Count == 1)
-                return -1;
-
-            return (int)_cell.bubble.Type * (int)Mathf.Pow(2, _similarBubbles.Count - 1);
+            return GetMatchingNeighbours(_cell, _cell.bubble.Type);
         }
     }
 }
