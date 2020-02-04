165aeb71a8c5be0571bee0a781838e389bc2199e
Saurabh S <saurabhs@mailbox.org>
2020-02-05 04:41:09 +0530

    Add beautiful UI

--- Assets/Scripts/UI/ScoreUI.cs
+++ Assets/Scripts/UI/ScoreUI.cs
@@ -18,7 +18,7 @@ namespace BubblePops.UI
         /// ref to TMP Text component of multiplier
         /// </summary>
         [SerializeField] private TextMeshProUGUI _multiplier = null;
-        
+
         /// <summary>
         /// ref to pause button in main scene UI
         /// </summary>
@@ -49,6 +49,41 @@ namespace BubblePops.UI
         /// </summary>
         [SerializeField] private ParticleSystem _particleSystem = null;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        [SerializeField] private GameObject _levelUpPopup = null;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [SerializeField] private Button _continueOnLevelUp = null;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [SerializeField] private GameObject _purrfectScore = null;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [SerializeField] private float _purrfectSpped = 1f;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private float _purrfectPopupDelay = 1.5f;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private Quaternion _purrfectOriginalRotation;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private Quaternion _purrfectTargetRotation;
+
         private void Start()
         {
             var scoreManager = FindObjectOfType<ScoreManager>();
@@ -62,9 +97,16 @@ namespace BubblePops.UI
             _score.text = scoreManager.Score.ToString();
             _currentLevel.text = scoreManager.Level.ToString();
             _nextLevel.text = (scoreManager.Level + 1).ToString();
+
+            _continueOnLevelUp.onClick.AddListener(OnContinueAfterLevelUpPopup);
+
+            var matchColor = FindObjectOfType<MatchColors>();
+            matchColor.on2048Reached.AddListener(OnPurrfectScore);
+
+            _purrfectOriginalRotation = _purrfectScore.transform.rotation;
         }
 
-        private void OnEnable() => _pause.onClick.AddListener(() => OnPause() );
+        private void OnEnable() => _pause.onClick.AddListener(() => OnPause());
 
         private void OnDisable() => _pause.onClick.RemoveAllListeners();
 
@@ -78,9 +120,11 @@ namespace BubblePops.UI
             _nextLevel.text = (level + 1).ToString();
 
             _particleSystem.Play();
+
+            _levelUpPopup.SetActive(true);
         }
 
-        public void UpdateFillbar(float fillAmount) 
+        public void UpdateFillbar(float fillAmount)
         {
             StartCoroutine(AnimateFillbar(fillAmount, 0.5f));
         }
@@ -99,7 +143,7 @@ namespace BubblePops.UI
         private IEnumerator ResetFillAmount(float decrement)
         {
             _levelFiller.fillAmount -= decrement * Time.deltaTime;
-            
+
             yield return new WaitForEndOfFrame();
 
             if(_levelFiller.fillAmount > 0)
@@ -117,5 +161,42 @@ namespace BubblePops.UI
             else if(_levelFiller.fillAmount > fillAmount)
                 _levelFiller.fillAmount = fillAmount;
         }
+
+        private void OnContinueAfterLevelUpPopup() => _levelUpPopup.SetActive(false);
+
+        private void OnPurrfectScore(int value)
+        {
+            _purrfectTargetRotation = Quaternion.Euler(0, 0, 0);
+
+            InvokeRepeating("AnimatePurrfectScore", 0f, Time.deltaTime);
+
+            StartCoroutine(CompleteAnimation());
+        }
+
+        private void AnimatePurrfectScore()
+        {
+            _purrfectScore.transform.rotation = Quaternion.Slerp(_purrfectScore.transform.rotation, _purrfectTargetRotation, _purrfectSpped * Time.deltaTime);
+        }
+
+        private IEnumerator CompleteAnimation()
+        {
+            yield return new WaitForSeconds(_purrfectPopupDelay);
+
+            CancelInvoke("AnimatePurrfectScore");
+            _purrfectScore.transform.rotation = _purrfectTargetRotation;
+
+            var euler = _purrfectOriginalRotation.eulerAngles;
+            _purrfectTargetRotation = Quaternion.Euler(euler.x, euler.y, -euler.z);
+
+            InvokeRepeating("AnimatePurrfectScore", 0f, Time.deltaTime);
+
+            yield return new WaitForSeconds(_purrfectPopupDelay);
+
+            CancelInvoke("AnimatePurrfectScore");
+            _purrfectScore.transform.rotation = _purrfectOriginalRotation;
+        }
+
+        [NaughtyAttributes.Button]
+        private void TestPurrfectScore() => OnPurrfectScore(100);
     }
 }
