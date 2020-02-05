6c0264d56f3c5ff8629a158ea619fdb9820d6b7c
Saurabh S <saurabhs@mailbox.org>
2020-02-05 17:52:03 +0530

    Add menu

---
+++ Assets/Scripts/Menu/MenuUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace BubblePops.Menu
+{
+    public class MenuUI : MonoBehaviour
+    {
+        [SerializeField] private Button _play = null;
+
+        private void Awake() => _play.onClick.AddListener(() => UnityEngine.SceneManagement.SceneManager.LoadScene("Main"));
+    }
+}
